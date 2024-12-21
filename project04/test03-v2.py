f7d8fd1363aaf78c80ad08d1a9da31de58854197
Sunw00ha <89060602+Sunw00ha@users.noreply.github.com>
2024-12-20 21:03:20 -0600

    Add files via upload

---
+++ project04/test03-v2.py
@@ -0,0 +1,24 @@
+## test03-v2.py ##
+print('starting')
+
+x = 456
+z = x * 10
+z = z * 5
+mytrue = True
+mytrue = 0
+mytrue = mytrue - 1
+apple = 9102 ** 2
+
+pass
+
+print(x)
+x = 43.56
+print("x is now")
+print(x)
+print(z)
+y = 87
+z = "overwriting with a string"
+apple = 1.23498
+z = z + " and more string"
+
+print('done')
